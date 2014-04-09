635b1d0aa6ab6b697f5e1bd2e7b194e72d94ac25
Pro9 <odeh_sam@hotmail.com>
2014-04-09 15:37:42 +0200

    added placeholder fragments

---
+++ app/src/main/java/se/group3/navigatorslittlehelper/app/ReadFragment.java
@@ -0,0 +1,22 @@
+package se.group3.navigatorslittlehelper.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReadFragment extends Fragment {
+
+    public ReadFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_read, container, false);
+
+        return rootView;
+    }
+
+}
