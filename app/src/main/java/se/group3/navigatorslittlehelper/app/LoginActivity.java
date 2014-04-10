aa12ef9031b4ea8dbaf1c4e2d4ce42ab712a20af
Viktor Nyblom <viktor.nyblom@gmail.com>
2014-04-10 11:55:09 +0200

    Updatede design of login view.

--- app/src/main/java/se/group3/navigatorslittlehelper/app/LoginActivity.java
+++ app/src/main/java/se/group3/navigatorslittlehelper/app/LoginActivity.java
@@ -1,17 +1,16 @@
 package se.group3.navigatorslittlehelper.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.kohsuke.github.GHMyself;
 import org.kohsuke.github.GitHub;
 
 import java.io.IOException;
@@ -19,7 +18,7 @@ import java.io.IOException;
 /**
  * Created by qw4z1 on 4/8/14.
  */
-public class LoginActivity extends ActionBarActivity{
+public class LoginActivity extends ActionBarActivity implements View.OnClickListener{
     private static final String TAG = LoginActivity.class.getSimpleName();
 
     private EditText mUsernameEdittext;
@@ -29,17 +28,15 @@ public class LoginActivity extends ActionBarActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
 
         mUsernameEdittext = (EditText) findViewById(R.id.login_username_edittext);
         mPasswordEdittext = (EditText) findViewById(R.id.login_password_edittext);
-        final Button button = (Button) findViewById(R.id.login_button);
+        final Button loginButton = (Button) findViewById(R.id.login_button);
+        final Button forgotButton = (Button) findViewById(R.id.login_forgot_password_button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
+        loginButton.setOnClickListener(this);
+        forgotButton.setOnClickListener(this);
     }
 
     private void login() {
@@ -51,6 +48,26 @@ public class LoginActivity extends ActionBarActivity{
         }
     }
 
+    private void forgotPassword() {
+        String url = getString(R.string.github_forgot_password_string);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                login();
+                break;
+            case R.id.login_forgot_password_button:
+                forgotPassword();
+                break;
+        }
+
+    }
+
     private class LoginTask extends AsyncTask<String, Void, Boolean> {
 
         @Override
@@ -58,9 +75,6 @@ public class LoginActivity extends ActionBarActivity{
             boolean returnValue;
             try {
                 GitHub github = GitHub.connectUsingPassword(params[0], params[1]);
-                Log.d(TAG, "github is null: " + (github== null));
-                GHMyself myself = github.getMyself();
-                Log.d(TAG, "myself is null: " + (myself== null));
                 returnValue = github.isCredentialValid();
             } catch (IOException e) {
                 return false;
