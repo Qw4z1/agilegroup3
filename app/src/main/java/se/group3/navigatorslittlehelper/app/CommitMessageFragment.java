dc4cc0eb80f515cb18dc77c4c11781b1f99492f4
Viktor Nyblom <viktor.nyblom@gmail.com>
2014-05-09 12:01:52 +0200

    Styling overhaul

--- app/src/main/java/se/group3/navigatorslittlehelper/app/CommitMessageFragment.java
+++ app/src/main/java/se/group3/navigatorslittlehelper/app/CommitMessageFragment.java
@@ -2,25 +2,16 @@ package se.group3.navigatorslittlehelper.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ListView;
 
 import org.kohsuke.github.GHCommit;
-import org.kohsuke.github.GHRepository;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.Map;
 
 import se.group3.navigatorslittlehelper.app.adapter.CommitMessageItemCustomAdapter;
-import se.group3.navigatorslittlehelper.app.adapterobjects.ObjectChooseRepositoryItem;
 import se.group3.navigatorslittlehelper.app.adapterobjects.ObjectCommitMessageItem;
 import se.group3.navigatorslittlehelper.app.handler.GitHubHandler;
 
@@ -37,14 +28,6 @@ public class CommitMessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_commit_message, container, false);
         listview = (ListView) rootView.findViewById(R.id.commit_message_list_view);
-
-        /*
-        ArrayList<ObjectCommitMessageItem> commitlist = new ArrayList<ObjectCommitMessageItem>();
-        commitlist.add(new ObjectCommitMessageItem("Commit message 1","Author 1",new Date(114, 3, 11, 8, 20, 20)));
-        commitlist.add(new ObjectCommitMessageItem("Commit message 2","Author 2",new Date(114, 3, 11, 6, 20, 20)));
-        commitlist.add(new ObjectCommitMessageItem("Commit message 3","Author 3",new Date(114, 3, 11, 4, 20, 20)));
-        commitlist.add(new ObjectCommitMessageItem("Commit message 4","Author 4",new Date(114, 3, 11, 9, 20, 20)));
-        */
 
         adapter = new CommitMessageItemCustomAdapter(getActivity(), R.layout.commit_message_list_item, new ArrayList<ObjectCommitMessageItem>());
         listview.setAdapter(adapter);
@@ -83,8 +66,6 @@ public class CommitMessageFragment extends Fragment {
     }
 
     private void updateList(){
-        //adapter.clear();
         adapter.notifyDataSetChanged();
-        //listview.invalidateViews();
     }
 }
